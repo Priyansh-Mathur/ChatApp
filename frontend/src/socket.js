39ca945d1eed0837cfe13e3e30e592e864718cd2
Priyansh-Mathur <priyanshmathur1105@gmail.com>
2026-06-12 23:47:24 +0530

    Update frontend backend URL

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-  autoConnect: false,
-});
+const socket = io(
+  process.env.REACT_APP_SOCKET_URL || "https://chatapp-cpl4.onrender.com",
+  {
+    autoConnect: false,
+  }
+);
 
 export default socket;
